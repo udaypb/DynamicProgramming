5d833f82acebd2b7ec8756614397d968200f5ffd
udaypb <uday.pb26@gmail.com>
2021-09-05 14:10:42 -0700

    fib recursive

--- fibonacci.py
+++ fibonacci.py
@@ -3,8 +3,8 @@ import time
 class fibonacci:
     def __init__(self, n):
         self.n = n
-        self.timeout = time.time() + 10 # 1 minute for each iteration
-       
+        self.timeout = time.time() + 5 # 1 minute for each iteration
+        self.timeoutFlag = False
 
     def __details__(self, n):
         self.__init__(n)
@@ -20,9 +20,12 @@ class fibonacci:
     def fib_recursive(self, n):
         if n <= 2:
             return 1
-
-        return self.fib_recursive(n - 2) + self.fib_recursive(n - 1)
-        
+        if time.time() < self.timeout:
+            return self.fib_recursive(n - 2) + self.fib_recursive(n - 1)
+        else:
+            self.timeoutFlag = True
+            return -1
+            
     
 
 n = [4, 5, 6, 10, 1000]
@@ -30,5 +33,10 @@ i = 0
 while i < len(n):
     num = n[i]
     fib = fibonacci(num)
-    print(" -> Calculating {} th fib number Recursively..........".format(num), fib.fib_recursive(num))
+    print(" -> Calculating {} th fib number Recursively..........".format(num))
+    ans = fib.fib_recursive(num)
+    if not fib.timeoutFlag:
+        print(ans)
+    else:
+        print('Timed out')
     i += 1
